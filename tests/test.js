a4d73dcccaea3232e7fdcc78497c6602da6d13ee
Devon Govett <devongovett@gmail.com>
2013-09-17 01:47:22 -0400

    Moar tests

--- tests/test.js
+++ tests/test.js
@@ -2,11 +2,17 @@ var glsl = require('../compiler');
 var assert = require('assert');
 var fs = require('fs');
 
+var BOILERPLATE = fs.readFileSync(__dirname + '/data/boilerplate.js', 'utf8');
+
 function compare(a, b) {
     assert.equal(a.replace(/[\s\n\r]*/g, ''), b.replace(/[\s\n\r]*/g, ''));
 }
 
-describe('glsl', function() {
+function checkMain(a, b) {
+    compare(a.split('\n').slice(5, -6).join('\n'), b);
+}
+
+describe('main function', function() {
     
     it('should throw an error without a main function', function() {
         assert.throws(function() {
@@ -33,7 +39,23 @@ describe('glsl', function() {
     });
     
     it('should generate asm.js boilerplate', function() {
-        compare(glsl.compile('void main() {}'), fs.readFileSync(__dirname + '/data/boilerplate.js', 'utf8'));
+        compare(glsl.compile('void main() {}'), BOILERPLATE);
+    });
+    
+});
+
+describe('variable declarations', function() {
+    
+    it('should default ints to 0', function() {
+        checkMain(glsl.compile('void main() { int test; }'), 'function main() { var test = 0; }');
+    });
+    
+    it('should default floats to 0.0', function() {
+        checkMain(glsl.compile('void main() { float test; }'), 'function main() { var test = (0.0); }');
+    });
+    
+    it('should default bools to 0 (false)', function() {
+        checkMain(glsl.compile('void main() { bool test; }'), 'function main() { var test = 0; }');
     });
     
 });
